5d9c29dc615ad3a4f8237e9cdfcbe39ac3f71e6a
Shenghang Zhu <443880@student.fontys.nl>
2023-09-19 14:48:37 +0200

    update

--- crdt-demo/src/store.ts
+++ crdt-demo/src/store.ts
@@ -15,21 +15,43 @@
 // export const connect = () => webrtcProvider.connect();
 
 import { syncedStore, getYjsDoc } from '@syncedstore/core';
-import { WebsocketProvider } from 'y-websocket';
+import { WebrtcProvider } from 'y-webrtc';
 
 // (optional, define types for TypeScript)
 type Todo = { completed: boolean; title: string };
 
 // Create your SyncedStore store
-export const store = syncedStore({ todos: [] as Todo[] });
+export const store = syncedStore({ todos: [] as Todo[], fragment: 'xml' });
 
 // Create a document that syncs automatically using Y-WebRTC
 const doc = getYjsDoc(store);
-export const wsProvider = new WebsocketProvider(
-  'ws://localhost:3001',
-  'room-1',
-  doc
-);
 
-export const disconnect = () => wsProvider.disconnect();
-export const connect = () => wsProvider.connect();
+// Specify the local signaling server
+const signalingServers = ['ws://localhost:4444'];
+
+export const webrtcProvider = new WebrtcProvider('syncedstore-todos', doc, {
+  signaling: signalingServers,
+});
+
+export const disconnect = () => webrtcProvider.disconnect();
+export const connect = () => webrtcProvider.connect();
+
+// import { syncedStore, getYjsDoc } from '@syncedstore/core';
+// import { WebsocketProvider } from 'y-websocket';
+
+// // (optional, define types for TypeScript)
+// type Todo = { completed: boolean; title: string };
+
+// // Create your SyncedStore store
+// export const store = syncedStore({ todos: [] as Todo[] });
+
+// // Create a document that syncs automatically using Y-WebRTC
+// const doc = getYjsDoc(store);
+// export const wsProvider = new WebsocketProvider(
+//   'ws://localhost:3001',
+//   'room-1',
+//   doc
+// );
+
+// export const disconnect = () => wsProvider.disconnect();
+// export const connect = () => wsProvider.connect();
