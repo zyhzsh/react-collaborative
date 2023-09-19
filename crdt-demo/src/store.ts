160cea656568de4ab87ef858cc98b6d796090823
Shenghang Zhu <443880@student.fontys.nl>
2023-09-19 15:39:55 +0200

    websocket example

--- crdt-demo/src/store.ts
+++ crdt-demo/src/store.ts
@@ -15,7 +15,7 @@
 // export const connect = () => webrtcProvider.connect();
 
 import { syncedStore, getYjsDoc } from '@syncedstore/core';
-import { WebrtcProvider } from 'y-webrtc';
+import { WebsocketProvider } from 'y-websocket';
 
 // (optional, define types for TypeScript)
 type Todo = { completed: boolean; title: string };
@@ -23,35 +23,17 @@ type Todo = { completed: boolean; title: string };
 // Create your SyncedStore store
 export const store = syncedStore({ todos: [] as Todo[], fragment: 'xml' });
 
-// Create a document that syncs automatically using Y-WebRTC
+// Create a document that syncs automatically using y-websocket
 const doc = getYjsDoc(store);
 
-// Specify the local signaling server
-const signalingServers = ['ws://localhost:4444'];
+// Specify the local WebSocket server
+const websocketServerURL = 'ws://localhost:1234';
 
-export const webrtcProvider = new WebrtcProvider('syncedstore-todos', doc, {
-  signaling: signalingServers,
-});
+export const websocketProvider = new WebsocketProvider(
+  websocketServerURL,
+  '',
+  doc
+);
 
-export const disconnect = () => webrtcProvider.disconnect();
-export const connect = () => webrtcProvider.connect();
-
-// import { syncedStore, getYjsDoc } from '@syncedstore/core';
-// import { WebsocketProvider } from 'y-websocket';
-
-// // (optional, define types for TypeScript)
-// type Todo = { completed: boolean; title: string };
-
-// // Create your SyncedStore store
-// export const store = syncedStore({ todos: [] as Todo[] });
-
-// // Create a document that syncs automatically using Y-WebRTC
-// const doc = getYjsDoc(store);
-// export const wsProvider = new WebsocketProvider(
-//   'ws://localhost:3001',
-//   'room-1',
-//   doc
-// );
-
-// export const disconnect = () => wsProvider.disconnect();
-// export const connect = () => wsProvider.connect();
+export const disconnect = () => websocketProvider.disconnect();
+export const connect = () => websocketProvider.connect();
